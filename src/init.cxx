65fb2feba56c2015996f0d6ceddef9542b3b57f8
fifr <fifr@noreply.codeberg.org>
2025-04-28 08:46:26 +0200

    Register filter and `ScanImage` as QML types
    FossilOrigin-Name: 4e3f0f8baca564513607aac0682fc5179fe98952

--- src/init.cxx
+++ src/init.cxx
@@ -28,6 +28,11 @@
 #include "RotImage.hxx"
 #include "ZoomImage.hxx"
 
+#include "ScanImage.hxx"
+#include "RotateFilter.hxx"
+#include "CutFilter.hxx"
+#include "ColorizeFilter.hxx"
+
 #include "Document.hxx"
 #include "DocumentList.hxx"
 #include "Global.hxx"
@@ -49,12 +54,17 @@ void init_app(QGuiApplication& app, QQmlEngine& engine)
     qmlRegisterType<PlainImage>("Fotokopierer", 1, 0, "PlainImage");
     qmlRegisterType<RotImage>("Fotokopierer", 1, 0, "RotImage");
     qmlRegisterType<ZoomImage>("Fotokopierer", 1, 0, "ZoomImage");
+    qmlRegisterType<ScanImage>("Fotokopierer", 1, 0, "ScanImage");
     qmlRegisterUncreatableType<Document>(
         "Fotokopierer",
         1,
         0,
         "Document",
         QStringLiteral("Document cannot be used as QML component"));
+
+    qmlRegisterUncreatableType<RotateFilter>("Fotokopierer", 1, 0, "RotateFilter", QStringLiteral("RotateFilter cannot be used as QML component"));
+    qmlRegisterUncreatableType<CutFilter>("Fotokopierer", 1, 0, "CutFilter", QStringLiteral("RotateFilter cannot be used as QML component"));
+    qmlRegisterUncreatableType<ColorizeFilter>("Fotokopierer", 1, 0, "ColorizeFilter", QStringLiteral("RotateFilter cannot be used as QML component"));
 
     app.setApplicationName(QStringLiteral("Fotokopierer"));
     app.setApplicationVersion(QStringLiteral(QT_VERSION_STR));
