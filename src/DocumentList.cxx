cdda1a0a756e3b9f89a0a2147fc3da4620519bcd
fifr <fifr@noreply.codeberg.org>
2025-04-28 08:46:26 +0200

    DocumentList: use `QStringList` instead of `QVariantList`
    FossilOrigin-Name: 587b68c20edf69d87834ccd8e0d169ef3a903b13

--- src/DocumentList.cxx
+++ src/DocumentList.cxx
@@ -72,10 +72,10 @@ QVariant DocumentList::data(const QModelIndex &index, int role) const
         case NumPagesRole: return d->docs.size();
         case DocumentRole: return QVariant::fromValue(d->docs[index.row()].data());
         case ThumbnailsRole: {
-            QVariantList thumbs;
+            QStringList thumbs;
             auto &doc = d->docs[index.row()];
             for (int i = 0, n = std::min(doc->numPages(), 3); i < n; i++) {
-                thumbs.push_back(QUrl::fromLocalFile(doc->page(i).thumbnail()));
+                thumbs.push_back(QUrl::fromLocalFile(doc->page(i).thumbnail()).toString());
             }
             return thumbs;
         }
