5b861a081b3af0d941884cbb1d6e193617b62165
fifr <fifr@noreply.codeberg.org>
2025-04-28 08:46:27 +0200

    EdgeDetection: use cv::Mat instead of QImage
    FossilOrigin-Name: 9b02b39f23d1b928aa845c99d9528577e4f8e21b

--- src/CutView.cxx
+++ src/CutView.cxx
@@ -17,6 +17,7 @@
 
 #include "CutView.hxx"
 
+#include "Convert.hxx"
 #include "EdgeDetection.hxx"
 #include "ScanImage.hxx"
 #include "Scanner.hxx"
@@ -426,7 +427,7 @@ void CutView::onNewImage()
             d->orientationChangedConnection = connect(d->scanImage.get(), &ScanImage::orientationChanged, this, &CutView::onOrientationChanged);
 
             // ... and its edge-detection data structure
-            d->edges = std::make_unique<EdgeDetection>(d->scanImage->original());
+            d->edges = std::make_unique<EdgeDetection>(QImageToCvMat(d->scanImage->original()));
             connect(d->edges.get(), &EdgeDetection::hasAutoDetectionChanged, this, &CutView::hasAutoSelectionChanged);
             connect(d->edges.get(), &EdgeDetection::isAutoDetectionRunningChanged, this, &CutView::isAutoDetectionRunningChanged);
 
